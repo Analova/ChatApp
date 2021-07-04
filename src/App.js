ff7d09025ce46239633a64a47a9b7111baf00fc3
Ana Nikolova <ani.nik19@gmail.com>
2021-07-04 17:52:49 +0200

    conected to the ChatEngine

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import {ChatEngine} from "react-chat-engine"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import ChatFeed from "./ChatFeed"
+
+import "./App.css"
+
+const App=()=>{
+    return(
+        <ChatEngine
+        height="100vh"
+        projectID="5b7966ca-ef56-45d7-9fd4-afacd269ab66"
+        userName="Ani"
+        userSecret="12345"
+        renderChatFeed={(chatAppProps)=><ChatFeed {...chatAppProps}/>}
+        
+        />
+    )
 }
 
 export default App;
